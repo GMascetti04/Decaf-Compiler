7438c240c49e4cc79f8a89aff8b1d07aaeb75159
GMascetti04 <giovanni.mascetti54@gmail.com>
2024-12-21 16:17:13 -0500

    Added flag to allow compiler to read source program from stdin

--- compiler/decaf_compiler.py
+++ compiler/decaf_compiler.py
@@ -2,50 +2,59 @@ import sys
 import decaf_lexer
 import decaf_parser
 import decaf_codegen
+import argparse
 
 def modify_file_extension(file_name): 
     if file_name.endswith('.decaf'): 
         return file_name[:-6] + '.ami'
     return file_name + '.ami'
 
-def main():
-    if len(sys.argv) < 2:
-        print("\033[31mError: No input file specified\033[0m")
-        return
+def compile(infile, outfile):
+        
+    data = infile.read()
     
-    try:
-        file = open(sys.argv[1], 'r', encoding="utf-8")
-        data = file.read()
+    ast = decaf_parser.parse_file(data)
+    
+    if ast == None:
+        print("\033[31mCompilation Failed\033[0m", file=sys.stderr)
+        sys.exit(1)
         
-        ast = decaf_parser.parse_file(data)
+    gen = decaf_codegen.AbstractCodeGenerator(ast)
+    program = gen.generate_code()
         
-        if ast == None:
-            print("\033[31mCompilation Failed\033[0m")
-            sys.exit(1)
-            
-        elif len(sys.argv) >= 3 and sys.argv[2] == '--show-ast':
-            print(ast)
-            
-        print(ast)
-        
-        gen = decaf_codegen.AbstractCodeGenerator(ast)
-        program = gen.generate_code()
-        
-        
+    program.print_to_file(outfile)
+    
 
-        output_file_name = modify_file_extension(sys.argv[1])
+    print(f'\033[32mCompilation Succeeded\033[0m', file=sys.stderr)
 
-        with open(output_file_name, 'w') as file:
-            program.print_to_file(file)
-        
+    
 
-        print(f'\033[32mCompilation Succeeded - Program written to "{output_file_name}".\033[0m (Run with "--show-ast" to print AST)')
 
-    except OSError:
-        print("Could not open file")
 
 
 
 if __name__ == "__main__":
-   main()
     
+    parser = argparse.ArgumentParser(description="A compiler for the programming language Decaf")
+    
+    parser.add_argument("--infile", type=str, help="input file - will read from stdin if none specified")
+    
+    
+    
+    
+    args = parser.parse_args()
+    
+    if args.infile == None:
+        compile(sys.stdin, sys.stdout)
+    else:
+        try:
+            read_file = open(args.infile, 'r')
+            out_file = open(modify_file_extension(args.infile), 'w')
+            compile(read_file, out_file)
+
+            
+        except Exception:
+           print("Could not open file")
+    
+   
+            
